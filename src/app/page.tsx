fcc5796426513aac0d02179ce8b32c9fa13d3ab8
Anderson Tucunduva <andersonpreuhs@gmail.com>
2024-03-01 20:20:59 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="h-screen p-10">
-        hello world
-        <h2>olaaaaaaaa mundo</h2>
-      </h1>
+      <h1>olaaaaaaaa mundo</h1>
     </>
   )
 }
