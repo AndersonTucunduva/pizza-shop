6705918ba22938215e14aaa4abdcefa72bc2fa14
Anderson Tucunduva <andersonpreuhs@gmail.com>
2024-03-11 15:20:59 -0300

    nav ready

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,13 +1,14 @@
+import Header from '@/components/header'
+
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <section>
-      <div>Testando layout dashboard</div>
-
-      {children}
-    </section>
+    <div className="flex min-h-screen flex-col antialiased">
+      <Header />
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">{children}</div>
+    </div>
   )
 }
---
+++ src/components/nav-link.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import Link, { LinkProps } from 'next/link'
+import { usePathname } from 'next/navigation'
+import { ReactNode } from 'react'
+
+export type NavLinkProps = LinkProps & {
+  children: ReactNode
+}
+
+export default function NavLink(props: NavLinkProps) {
+  const pathName = usePathname()
+  console.log(pathName)
+  console.log(props.href)
+  return (
+    <Link
+      data-current={pathName === props.href}
+      className="flex items-center gap-1.5  text-sm font-medium text-muted-foreground hover:text-foreground data-[current=true]:text-foreground"
+      {...props}
+    />
+  )
+}
