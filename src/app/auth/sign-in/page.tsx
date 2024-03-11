a7f8de496c71c4b49ddb481c5c55fed8557737a5
Anderson Tucunduva <andersonpreuhs@gmail.com>
2024-03-11 11:56:33 -0300

    sing-in sign-up ready

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -7,6 +7,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { toast } from 'sonner'
+import Link from 'next/link'
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -29,12 +30,17 @@ export default function SignIn() {
 
   return (
     <div className="p-8">
+      <Button asChild className="absolute right-8 top-8" variant={'ghost'}>
+        <Link href="/auth/sign-up">Novo estabelecimento</Link>
+      </Button>
       <div className="flex w-[350px] flex-col justify-center gap-6">
         <div className="flex flex-col gap-2 text-center">
           <h1 className="text-2xl font-semibold tracking-tight">
             Acessar painel
           </h1>
-          <p>Acompanhe suas vendas pelo painel do parceiro</p>
+          <p className="text-sm text-muted-foreground">
+            Acompanhe suas vendas pelo painel do parceiro
+          </p>
         </div>
         <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
           <div className="space-y-2">
