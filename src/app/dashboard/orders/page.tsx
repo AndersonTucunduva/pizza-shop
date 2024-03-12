595e91f8b82adbc44636e99bae9bb51131973e67
Anderson Tucunduva <andersonpreuhs@gmail.com>
2024-03-12 12:26:36 -0300

    table and filters ready

---
+++ src/app/dashboard/orders/page.tsx
@@ -0,0 +1,44 @@
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+import { OrderTableRow } from './order-table-row'
+import { OrderTableFilters } from './order-table-filters'
+
+export default function Orders() {
+  return (
+    <div>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tighter">Pedidos</h1>
+      </div>
+      <div className="mb-3 mt-3 flex flex-col gap-2">
+        <OrderTableFilters />
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[64px]"></TableHead>
+                <TableHead className="w-[140px]">Identificador</TableHead>
+                <TableHead className="w-[180px]">Realizado há</TableHead>
+                <TableHead className="w-[140px]">Status</TableHead>
+                <TableHead>Cliente</TableHead>
+                <TableHead className="w-[140px]">Total do pedido</TableHead>
+                <TableHead className="w-[164px]"></TableHead>
+                <TableHead className="w-[132px]"></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {Array.from({ length: 9 }).map((_, i) => {
+                return <OrderTableRow key={i} />
+              })}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
             <Home className="h-4 w-4" />
             Inicio
           </NavLink>
-          <NavLink href="/orders">
+          <NavLink href="/dashboard/orders">
             <UtensilsCrossed className="h-4 w-4" />
             Pedidos
           </NavLink>
